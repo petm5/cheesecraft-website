8984b6d043ebf316a19f1209dcf111e81f14813b
Peter Marshall <petms@proton.me>
2024-02-03 17:46:07 -0500

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Operation Cheesecake website";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+          ];
+        };
+      }
+    );
+}
